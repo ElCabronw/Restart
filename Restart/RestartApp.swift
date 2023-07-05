306c2f4d26ebf592d7a3ccc9169e4e80ace8d501
ElCabronw <lucasfuziyama@gmail.com>
2023-07-05 16:18:43 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Lucas Fuziyama on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
